bf234c737e2d8ac97368138090dabc88b3e7434b
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-03-13 08:11:37 -0700

    change schedule

--- reindex-db-job/jobs.tf
+++ reindex-db-job/jobs.tf
@@ -64,7 +64,7 @@ resource "google_cloud_scheduler_job" "scheduler" {
     job.name => job
   }
   name             = each.value.name
-  schedule         = "0 2 * * 7"
+  schedule         = "0 2 * * 0"
   time_zone        = "America/Vancouver"
   attempt_deadline = "320s"
   region           = var.region
