76c3aad9f46b7cda365bb4a24730228badc71cdb
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-03-13 00:17:30 -0700

    rename dir

---
+++ reindex-db-job/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  cloud {
+    organization = "BCRegistry"
+    workspaces {
+      name = "reindex-db-job-dev"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    google = ">= 3.3"
+    onepassword = {
+      source = "1Password/onepassword"
+      version = "1.1.4"
+    }
+  }
+
+}
+
+provider "google" {
+  project = var.environment.project_id
+}
