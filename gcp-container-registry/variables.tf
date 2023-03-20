260b572e32d6892bb5465e01896d745644cc7855
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-03-20 13:19:18 -0700

    terraform to upload docker images to gcp container registry

---
+++ gcp-container-registry/variables.tf
@@ -0,0 +1,27 @@
+variable "environment" {
+  type = object({
+    project_id     = string
+    registry       = string
+  })
+  description = "GCP project parameters"
+
+  default = {
+    project_id      = "c4hnrd-dev"
+    registry        = "gcr.io"
+  }
+}
+
+variable "images" {
+  type = list(string)
+
+  description = "images to maintain"
+
+  default = [
+    "tiangolo/uvicorn-gunicorn-fastapi:python3.8",
+    "nginx:1.18.0",
+    "nats-streaming:0.21.2",
+    "node:16.14.2",
+    "node:14.15.1",
+    "minio/minio:RELEASE.2018-07-13T00-09-07Z"
+  ]
+}
