8a554591ce8783b3d1bf64fb687cb2cfb541c89b
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-03-21 08:58:31 -0700

    add more images

--- gcp-container-registry/variables.tf
+++ gcp-container-registry/variables.tf
@@ -54,6 +54,14 @@ variable "images" {
       ocp_tag       = ":14.15.1"
     },
     {
+      image         = "solr"
+      ocp_tag       = ":6.6"
+    }.
+    {
+      image         = "docker.io/centos/postgresql-12-centos7"
+      ocp_tag       = "20200917-804ef01"
+    },
+    {
       image         = "image-registry.apps.silver.devops.gov.bc.ca/d893f6-tools/minio"
       ocp_tag       = "@sha256:6c7d8bac62177e836f88ef991362a02f5b10faa9cb92aeecd2d3f066042ba849"
       gcp_tag       = ":backup"
@@ -61,6 +69,21 @@ variable "images" {
     {
       image         = "image-registry.apps.silver.devops.gov.bc.ca/d893f6-tools/nats-streaming"
       ocp_tag       = "@sha256:883d4d7a47db4d1c911cf0c23dad30011b46727297d1158bede3d833455a49e4"
+      gcp_tag       = ":backup"
+    },
+    {
+      image         = "image-registry.apps.silver.devops.gov.bc.ca/f2b77c-tools/solr"
+      ocp_tag       = "@sha256:4a5cec90c431df6af36f40f73725a9568a6ab7f36fc1a2725abfa8d8ce52e16f"
+      gcp_tag       = ":backup"
+    },
+    {
+      image         = "image-registry.apps.silver.devops.gov.bc.ca/f2b77c-tools/namex-solr-base"
+      ocp_tag       = "@sha256:1291ad4242474401779f8472314fa42690f0be27534631eea93d41b883fdda7a"
+      gcp_tag       = ":backup"
+    },
+    {
+      image         = "image-registry.apps.silver.devops.gov.bc.ca/f2b77c-tools/fdw"
+      ocp_tag       = "@sha256:956e7855fe743859d47f7efd00e758c8e8947546119716008e839bed2bce51fe"
       gcp_tag       = ":backup"
     }
   ]
