1181ddcaed77e60303aa7f86e21f13711507570f
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-03-29 17:09:17 -0700

    add notify api

--- reports/notebookreport.py
+++ reports/notebookreport.py
@@ -1,37 +1,66 @@
 """s2i based launch script to run the notebook."""
-import fnmatch
+import requests
 import os
 import sys
 from datetime import datetime, timedelta
 import papermill as pm
-from flask import Flask, current_app
-from config import Config
 import glob
 import csv
+import base64
 
 
-def create_app(config=Config):
-    app = Flask(__name__)
-    app.config.from_object(config)
-    # db.init_app(app)
-    app.app_context().push()
-    return app
+def send_email(email: dict):
+    """Send the email."""
+    client = os.environ['NOTIFY_CLIENT']
+    secret = os.environ['NOTIFY_CLIENT_SECRET']
+    kc_url = os.environ['KC_URL']
+    notify_base_url = os.environ['NOTIFY_API_URL']
+    payload = "grant_type=client_credentials"
+    auth_str = client + ":" + secret
+    basic_hash = base64.b64encode(auth_str.encode()).decode()
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': f'Basic {basic_hash}'
+    }
+    response = requests.request("POST", kc_url, headers=headers, data=payload)
+    token = response.json()['access_token']
 
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+    url = notify_base_url + "/api/v2/notify/resend"
+    response = requests.request("POST", url, headers=headers)
 
-def send_email(subject, filename):
-    """STUB"""
-    print(subject)
-    print(filename)
+    resp = requests.post(
+        os.getenv('NOTIFY_API_URL', ''),
+        json=email,
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+    )
+    if resp.status_code != 200:
+        raise Exception('Unsuccessful response when sending email.')
 
 
 def processnotebooks():
     status = False
-    date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+    
     for file in  glob.glob('*.ipynb', recursive=True):
         note_book = os.path.basename(file)
+        subject = note_book.split('.ipynb')[0]
+        email = {
+            'recipients': os.getenv('REPORT_RECIPIENTS', 'Andriy.Bolyachevets@gov.bc.ca'),
+            'requestBy': os.getenv('SENDER_EMAIL', 'no-reply@gov.bc.ca'),
+            'content': {
+                'subject': subject,
+                'body': '',
+                'attachments': []
+            }
+        }
         try:
             pm.execute_notebook(file, os.getenv('DATA_DIR', '')+'temp.ipynb', parameters=None)
-            subject = note_book.split('.ipynb')[0]
             files = glob.glob(os.getenv('DATA_DIR', '') + '*.csv')
             filename = files[0]
             counter = 0
@@ -43,12 +72,21 @@ def processnotebooks():
                     if counter >= 9:
                         break
             filename = os.path.basename(filename)
-            send_email(subject, filename)
+            
             status = True
         except Exception:  # noqa: B902
-            send_email('Error processing notebook', note_book)
+            # send_email('Error processing notebook', note_book)
+            email = {
+                'recipients': os.getenv('ERROR_EMAIL_RECIPIENTS', 'Andriy.Bolyachevets@gov.bc.ca'),
+                'requestBy': os.getenv('SENDER_EMAIL', 'no-reply@gov.bc.ca'),
+                'content': {
+                    'subject': subject,
+                    'body': 'Failed to generate report',
+                }
+            }
         finally:
             os.remove(os.getenv('DATA_DIR', '')+'temp.ipynb')
+            send_email(email)
     return status
 
 
